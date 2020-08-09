12a3b1a8499337950254a8c45098b9fafd1310e2
luisfff29 <luisfff29@gmail.com>
2020-08-08 22:01:54 -0500

    get an index.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const fs = require("fs");
+
 const path = "./public/uploads";
 const app = express();
 const port = 3000;
 
+app.use(express.static("./public"));
+
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to Kenziegram!</h1>");
     fs.readdir(path, (err, files) => {
         console.log(files);
     });
