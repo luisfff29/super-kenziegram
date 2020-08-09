bbe26fb71ef202b460f3983f72241eaea119518a
luisfff29 <luisfff29@gmail.com>
2020-08-09 01:39:12 -0500

    display images from uploads folder

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const fs = require("fs");
+const multer = require("multer");
+const ejs = require("ejs");
 
 const path = "./public/uploads";
 const app = express();
 const port = 3000;
+templates = {
+    titleHeader: "Kenziegram Project",
+    title: "<h1>KenzieGram</h1>",
+};
+
+app.set("view engine", "ejs");
 
 app.use(express.static("./public"));
 
+fs.readdir(path, (err, files) => {
+    console.log(files);
+    templates.pics = files;
+    console.log(templates);
+});
+
 app.get("/", (req, res) => {
-    fs.readdir(path, (err, files) => {
-        console.log(files);
-    });
+    res.render("index", templates);
+});
+
+app.post("/upload", (req, res) => {
+    res.render("index", templates);
 });
 
 app.listen(port, () => {
