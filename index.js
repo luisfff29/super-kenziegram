17eacf8f507cfa90557428319baeed0ff0aee24a
luisfff29 <luisfff29@gmail.com>
2020-08-09 02:34:29 -0500

    upload picture and go back

--- index.js
+++ index.js
@@ -9,23 +9,27 @@ const port = 3000;
 templates = {
     titleHeader: "Kenziegram Project",
     title: "<h1>KenzieGram</h1>",
+    posted: false,
 };
+const upload = multer({ dest: path });
 
 app.set("view engine", "ejs");
 
 app.use(express.static("./public"));
 
 fs.readdir(path, (err, files) => {
-    console.log(files);
     templates.pics = files;
-    console.log(templates);
 });
 
 app.get("/", (req, res) => {
+    templates.posted = false;
     res.render("index", templates);
 });
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.single("myImage"), (req, res, next) => {
+    templates.posted = true;
+    templates.pics.unshift(req.file.filename);
+    templates.pic_uploaded = req.file.filename;
     res.render("index", templates);
 });
 
