95f6ed9d31c2647fa0f594391c0cfbf1f1d480c4
luisfff29 <luisfff29@gmail.com>
2020-08-12 05:29:24 -0500

    updated for deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 const path = "./public/uploads";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 templates = {
     titleHeader: "Kenziegram Project",
     title: "<h1>KenzieGram</h1>",
